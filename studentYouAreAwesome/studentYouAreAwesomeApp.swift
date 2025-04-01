d5f2aaeb5aab4e5fc7d90067feb43aeef2afee76
karriem-lang <karriemlang@proton.me>
2025-04-01 03:20:00 -0400

    Initial Commit

---
+++ studentYouAreAwesome/studentYouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  studentYouAreAwesomeApp.swift
+//  studentYouAreAwesome
+//
+//  Created by Karriem Lang on 4/1/25.
+//
+
+import SwiftUI
+
+@main
+struct studentYouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
