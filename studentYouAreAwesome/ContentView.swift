3c256e2a99ca91e0584410ce84dce6d5e3c77e16
karriem-lang <karriemlang@proton.me>
2025-04-05 00:14:45 -0400

    Arrays, multilineTextAlignment, Array Modifiers and Properties

--- studentYouAreAwesome/ContentView.swift
+++ studentYouAreAwesome/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
     @State private var imageNumber = 0
+    @State private var messageNumber = 0
     
     var body: some View {
         
@@ -23,15 +24,33 @@ struct ContentView: View {
                 .scaledToFit()
                 .clipShape(RoundedRectangle(cornerRadius: 30))
                 .shadow(radius: 30)
-          
+            
+            Text(message)
+                .font(.largeTitle)
+                .foregroundColor(.red)
+                .fontWeight(.heavy)
+                .multilineTextAlignment(.center)
+            
             Spacer()
             
             Button("Press Me!") {
+                let messages = ["You Are Awesome!",
+                                "You Are Great!",
+                                "Fabulous, That's You!",
+                                "You Are A Developer!",
+                                "You Make Me Smile!",
+                                "When The Genius Bar Needs Help, They Call You!"]
                 imageName = "image\(imageNumber)"
                 imageNumber += 1
                 
                 if imageNumber > 9 {
                     imageNumber = 0
+                }
+                message = messages[messageNumber]
+                messageNumber += 1
+                
+                if messageNumber == messages.count {
+                    messageNumber = 0
                 }
             }
             .buttonStyle(.borderedProminent)
